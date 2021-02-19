3865a1d364b36fee1a31a33f9e2aad38259c22ce
Makeshiftdev-c <abde_321@hotmail.com>
2021-02-19 21:58:21 +0000

    added exercise 1.8

---
+++ chapter1/exercise-1.8/GeoCircle.java
@@ -0,0 +1,8 @@
+public class GeoCircle {
+	public static void main(String[] args) {
+		System.out.print("Perimeter = ");
+		System.out.println(2 * 6.5 * 3.14159);
+		System.out.print("Area = ");
+		System.out.println(6.5 * 6.5 * 3.14159);
+	}
+}
