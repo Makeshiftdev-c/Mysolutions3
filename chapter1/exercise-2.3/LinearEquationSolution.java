d69dd6594e2f7bbfb48aa2a7fd6d9cf27f26efeb
Makeshiftdev-c <abde_321@hotmail.com>
2021-07-23 06:32:38 +0100

    added exercise 1.13

---
+++ chapter1/exercise-2.3/LinearEquationSolution.java
@@ -0,0 +1,10 @@
+public class LinearEquationSolution {
+	public static void main(String[] args) {
+		System.out.print("x = ");
+		System.out.print(((44.5 * 0.55) - (50.2 * 5.9)) /
+						   ((3.4 * 0.55) - (50.2 * 2.1)));
+		System.out.print(", y = ");
+		System.out.println(((3.4 * 5.9) - (44.5 * 2.1)) /
+						   ((3.4 * 0.55) - (50.2 * 2.1)));
+	}
+}
